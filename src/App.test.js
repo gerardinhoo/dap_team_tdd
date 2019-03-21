1454388417eb51b31f9e82c87ff8764077d4c79c
Gerard Eklu <gerard_eklu@homedepot.com>
2019-03-21 05:53:14 -0400

    Updated comments

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
-// ----- Test Anatomy ---- //
-
-// 1. "describe"  => describes What component aspect we are testing
-// 2. "it"        => describes the piece of functionality we're testing.
-// 3. "expect"    => An assertion that perfoms a check on a software output.
-
+// Basic Test with Jest and Enzyme
 describe("App component", () => {
   it("checks for the existence of App component", () => {
     const wrapper = shallow(<App />);
@@ -20,3 +15,9 @@ describe("App component", () => {
     expect(header).toHaveLength(1);
   });
 });
+
+// ----- Test Anatomy ---- //
+
+// 1. "describe"  => describes What component aspect we are testing
+// 2. "it"        => describes the piece of functionality we're testing.
+// 3. "expect"    => An assertion that perfoms a check on a software output.
